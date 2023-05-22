302ccdf7bf0311a505bc67def99a386dcb9398b3
Gary Zhang <garyzhang2002@hotmail.com>
2023-05-23 00:17:14 +0800

    add hive (rubbish implementation)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from game import runGames
-from ghostAgents import GhostAgent, GhostAgentSlightlyRandom
+from ghostAgents import GhostAgent, GhostAgentSlightlyRandom, GhostsAgent
 from playerAgents import KeyboardAgent, RandomAgent
 from multiAgents import GreedyAgent, AlphaBetaAgent, ExpectimaxAgent
 from reinforcementAgents import MCTSAgent, QLearningAgent
@@ -14,10 +14,10 @@ if __name__ == "__main__":
         layout=Layout(
             map_size = (40,40),
             tile_size = (30,30),
-            ghostNum = 10,
+            ghostNum = 4,
             player_pos = (20,20),
-            ghosts_pos = [(1,1),(2,1),(3,1),(4,1)]
+            ghosts_pos = [(1,1),(2,1)]
         ),
         player=GreedyAgent(),
-        ghosts=[GhostAgentSlightlyRandom(i+1) for i in range(7)]+ [GhostAgent(8), GhostAgent(9), GhostAgent(10)]
+        ghosts=GhostsAgent(7)
     )
