605f23b01e028d3701909cd05dfb3f68b6c04bed
great-luao <you@example.com>
2023-05-24 01:16:33 +0800

    fix bug in the game.py

--- main.py
+++ main.py
@@ -14,15 +14,15 @@ if __name__ == "__main__":
     map_size = Vector2d(10, 10)
     # playerAgent = DQNAgent(map_size)
     # playerAgent = QLearningAgent()
-    ghosts_pos = [(1,1),(1,4),(2,2),(4,1),(4,4)]
-    player_pos = Vector2d(3, 6)
-    playerAgent = AlphaBetaAgent()
+    ghosts_pos = []
+    player_pos = Vector2d(5, 5)
+    playerAgent = MaxScoreAgent()
     # playerAgent = pickle.load(open("SarsaLambdaAgent.pkl", "rb"))
-    ghostsAgent = [GhostAgent(i) for i in range(1, 4)]
+    ghostsAgent = [GhostAgent(i) for i in range(1, 6)]
     layout = Layout(
         map_size = map_size,
-        tile_size = (30,30),
-        ghostNum = 3,
+        tile_size = (10,10),
+        ghostNum = 5,
         player_pos = player_pos,
         ghosts_pos = ghosts_pos,
     )
@@ -34,8 +34,8 @@ if __name__ == "__main__":
     #     ghosts=ghostsAgent,
     #     numTrain=1000
     # )
-    # pickle.dump(playerAgent, open("QLearningAgent.pkl", "wb"))
-    # # pickle.dump(playerAgent, open("DQNAgent.pkl", "wb"))
+    pickle.dump(playerAgent, open("QLearningAgent.pkl", "wb"))
+    # pickle.dump(playerAgent, open("DQNAgent.pkl", "wb"))
     # pickle.dump(playerAgent, open("SarsaLambdaAgent.pkl", "wb"))
 
     runGames(
